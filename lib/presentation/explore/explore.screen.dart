6740a51263bf1eb656cc5e1e2e2ff5197f78d136
Saurav-Rana-June <sauravsevenjune@gmail.com>
2026-01-13 10:45:24 +0530

    updates: Added Discover Meditation Section and Video Section to the Explore Screen.

--- lib/presentation/explore/explore.screen.dart
+++ lib/presentation/explore/explore.screen.dart
@@ -16,80 +16,303 @@ class ExploreScreen extends GetView<ExploreController> {
       backgroundColor: Theme.of(context).primaryColorLight,
       appBar: buildAppbar(context),
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-          horizontal: Spacing.s8.symmetric.horizontal,
-          vertical: Spacing.s4.symmetric.horizontal,
-        ),
         child: Column(
           children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    buildFeatureTile(
-                      context,
-                      '\u{f5bb}',
-                      'Meditations',
-                    ), // Change Icon :- person-meditating
-                    buildFeatureTile(
-                      context,
-                      '\u{e480}',
-                      'Breathing',
-                    ), // Change Icon :- face-exhaling
-                    buildFeatureTile(
-                      context,
-                      '\u{f1ea}',
-                      'Articles',
-                    ), // Change Icon :- newspaper
-                  ],
-                ),
-                Spacing.s8.h,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    buildFeatureTile(
-                      context,
-                      '\u{e4f3}',
-                      'Tests',
-                    ), // Change Icon :- flask-vial
-                    buildFeatureTile(
-                      context,
-                      '\u{f37e}',
-                      'Smart Journey',
-                    ), // Change Icon :- browser
-                    buildFeatureTile(
-                      context,
-                      '\u{f328}',
-                      'Notepad',
-                    ), // Change Icon :- clipboard
-                  ],
-                ),
-                Spacing.s8.h,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    buildFeatureTile(
-                      context,
-                      '\u{f4b4}',
-                      'Affirmations',
-                    ), // Change Icon :- comment-smile
-                    buildFeatureTile(
-                      context,
-                      '\u{e234}',
-                      'Quotes',
-                    ), // Change Icon :- quotes
-                    buildFeatureTile(
-                      context,
-                      '\u{f0eb}',
-                      'Tips',
-                    ), // Change Icon :- lightbulb
-                  ],
-                ),
-              ],
+            buildTopFeatureSection(context),
+            buildDiscoverMeditation(context),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: Spacing.s8.symmetric.horizontal,
+                vertical: Spacing.s4.symmetric.horizontal,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Video Sessions",
+                        textAlign: TextAlign.center,
+                        style: r18.copyWith(
+                          color: Theme.of(context).textTheme.bodyLarge!.color,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "View All",
+                        style: r14.copyWith(
+                          color: primary,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacing.s8.h,
+
+                  Column(
+                    children: [
+                      CustomPrimaryCard(
+                        child: Row(
+                          children: [
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 100,
+                                  height: 80,
+                                  child: Image.asset(
+                                    "assets/images/banner.png",
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                                Text(
+                                  '\u{f144}', // Change icon :- circle-play
+                                  style: TextStyle(
+                                    fontFamily: 'FontAwesomeSolid',
+                                    fontSize: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacing.s8.w,
+                            Expanded(
+                              child: SizedBox(
+                                height: 70,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                      style: r14.copyWith(
+                                        color: Theme.of(
+                                          context,
+                                        ).textTheme.bodyLarge!.color,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      "Stress Management",
+                                      textAlign: TextAlign.center,
+                                      style: r14.copyWith(
+                                        color: Theme.of(
+                                          context,
+                                        ).textTheme.bodySmall!.color,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Column buildDiscoverMeditation(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Spacing.s8.symmetric.horizontal,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Discover Meditations",
+                textAlign: TextAlign.center,
+                style: r18.copyWith(
+                  color: Theme.of(context).textTheme.bodyLarge!.color,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "View All",
+                style: r14.copyWith(
+                  color: primary,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Spacing.s4.h,
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(
+            horizontal: Spacing.s8.symmetric.horizontal,
+            vertical: Spacing.s4.symmetric.horizontal,
+          ),
+          child: Row(
+            children: [
+              buildDiscoverFeatureTile(
+                context,
+                '\u{f119}', // Change icon :- face-frown
+                'Stress Management',
+                '11 mins',
+              ),
+              Spacing.s8.w,
+              buildDiscoverFeatureTile(
+                context,
+                '\u{e027}', // Change icon :- rocket-launch
+                'Mood Boost Blueprint',
+                '25 mins',
+              ),
+              Spacing.s8.w,
+              buildDiscoverFeatureTile(
+                context,
+                '\u{e36a}', // Change icon :- face-anxious-sweat
+                'Anxiety Reducing Meditation',
+                '45 mins',
+              ),
+              Spacing.s8.w,
+              buildDiscoverFeatureTile(
+                context,
+                '\u{e480}', // Change icon :- face-exhaling
+                'Wim Hoff Technique',
+                '10 mins',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  InkWell buildDiscoverFeatureTile(
+    BuildContext context,
+    String icon,
+    String title,
+    String caption,
+  ) {
+    return InkWell(
+      onTap: () {},
+      child: CustomPrimaryCard(
+        child: SizedBox(
+          width: Get.width / 4.2,
+          child: Column(
+            children: [
+              Text(
+                icon,
+                style: TextStyle(
+                  fontFamily: 'FontAwesomeSolid',
+                  fontSize: 45,
+                  color: primary,
+                ),
+              ),
+              Spacing.s4.h,
+              Text(
+                title,
+                style: r14.copyWith(
+                  color: Theme.of(context).textTheme.bodyLarge!.color,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                caption,
+                textAlign: TextAlign.center,
+                style: r14.copyWith(
+                  color: Theme.of(context).textTheme.bodySmall!.color,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding buildTopFeatureSection(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: Spacing.s8.symmetric.horizontal,
+        vertical: Spacing.s4.symmetric.horizontal,
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildFeatureTile(
+                context,
+                '\u{f5bb}',
+                'Meditations',
+              ), // Change Icon :- person-meditating
+              buildFeatureTile(
+                context,
+                '\u{e480}',
+                'Breathing',
+              ), // Change Icon :- face-exhaling
+              buildFeatureTile(
+                context,
+                '\u{f1ea}',
+                'Articles',
+              ), // Change Icon :- newspaper
+            ],
+          ),
+          Spacing.s8.h,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildFeatureTile(
+                context,
+                '\u{e4f3}',
+                'Tests',
+              ), // Change Icon :- flask-vial
+              buildFeatureTile(
+                context,
+                '\u{f37e}',
+                'Smart Journey',
+              ), // Change Icon :- browser
+              buildFeatureTile(
+                context,
+                '\u{f328}',
+                'Notepad',
+              ), // Change Icon :- clipboard
+            ],
+          ),
+          Spacing.s8.h,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              buildFeatureTile(
+                context,
+                '\u{f4b4}',
+                'Affirmations',
+              ), // Change Icon :- comment-smile
+              buildFeatureTile(
+                context,
+                '\u{e234}',
+                'Quotes',
+              ), // Change Icon :- quotes
+              buildFeatureTile(
+                context,
+                '\u{f0eb}',
+                'Tips',
+              ), // Change Icon :- lightbulb
+            ],
+          ),
+          Spacing.s20.h,
+        ],
       ),
     );
   }
